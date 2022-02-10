19ffcfc3f605fe741a80a0da7ea7ad117bd0adbd
ahamednibir12 <ahamednibir639@gmail.com>
2022-02-11 00:33:20 +0600

    second commit

--- banking.js
+++ banking.js
@@ -1,7 +1,10 @@
 // handle deposit button event
 document.getElementById('diposit-button').addEventListener('click', function () {
-  // get the ammount deposit
+  // get the ammount deposit 
   const depositInput = document.getElementById('diposit-input');
   const depositAmount = depositInput.value;
   console.log(depositAmount);
+  const depositTotal = document.getElementById('diposit-total');
+  depositTotal.innerText = depositAmount;
+  depositInput.value = '';
 })
